1dcd930391c8990b61694c64797aead92f6fa480
Samantha Durant <samdurant.teacher@gmail.com>
2019-07-05 17:07:38 -0700

    Added a JS function to test if the clicked cards match

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 $(document).ready(initializeApp);
 
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+
 function initializeApp(){
   $('.card').click(handleCardClick)
 }
 
 function handleCardClick(event){
-  console.log(event);
+  $(event.currentTarget).children('.side1').addClass('hidden');
+
+  if (firstCardClicked === null){
+    firstCardClicked = $(event.currentTarget);
+  } else {
+    secondCardClicked = $(event.currentTarget);
+  }
+
+  if (firstCardClicked.find('*').css('background-image') === secondCardClicked.find('*').css('background-image')){
+    console.log("images match");
+  }
+
 }
